e071d2784914dcf3786ac4b7ba45ddb37f667e2d
natemek <natnaelmek@gmail.com>
2020-05-14 14:20:26 -0400

    github pages deploy

---
+++ build/precache-manifest.19e4ed33d8ae62bc26e6d38a7076cded.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd2bcf2984752456dd5bd7209e36082f",
+    "url": "/Whack-A-Mole/index.html"
+  },
+  {
+    "revision": "9ed658c1f967942f03a6",
+    "url": "/Whack-A-Mole/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "dfcdb4f41434c68fd579",
+    "url": "/Whack-A-Mole/static/css/main.51640a63.chunk.css"
+  },
+  {
+    "revision": "9ed658c1f967942f03a6",
+    "url": "/Whack-A-Mole/static/js/2.f6a83161.chunk.js"
+  },
+  {
+    "revision": "b73ad1b482cbb9c3a636b8aae2fa9aff",
+    "url": "/Whack-A-Mole/static/js/2.f6a83161.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfcdb4f41434c68fd579",
+    "url": "/Whack-A-Mole/static/js/main.7b626f7b.chunk.js"
+  },
+  {
+    "revision": "a4f86c5c100741d83a69",
+    "url": "/Whack-A-Mole/static/js/runtime-main.fc6eec62.js"
+  },
+  {
+    "revision": "cbdc96670be6d0be522f6210391cc87b",
+    "url": "/Whack-A-Mole/static/media/mole.cbdc9667.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.046c54846dfff4a97dc6dd505359129b.js"
+  "/Whack-A-Mole/precache-manifest.19e4ed33d8ae62bc26e6d38a7076cded.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Whack-A-Mole/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
